6e0a66d04b2cbf06720c2eb946d2586ca12da075
SamaySa2003 <samay.desacunhasc@gmail.com>
2022-11-12 19:31:48 -0300

    Finalizado back-end do registro.

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BEM VINDO A EPIC VAPOUR!</title>
+    <link rel="stylesheet" href="./style/style.css">
+</head>
+<body>
+    <nav>
+        <div class="navbar">
+            <ul>
+                <li><a href="index.php" >Home</a></li>
+                <li><a href="javascript:void(0)">Comunidade</a></li>
+                <li><a href="javascript:void(0)">Jogos</a></li>
+                <li><a href="javascript:void(0)">Contato</a></li>
+                <li style="float:right"><a href="registro.php">Registro</a></li>
+                <li style="float:right"><a href="javascript:void(0)" class="active">Entrar</a></li>
+            </ul>
+        <div class="navbar">
+    </nav>
+</body>
+</html>
