a69ea55eed49316bb7ba8d2b90578ca4cf1c7b68
LuanMacedo074 <lu.m3105@gmail.com>
2022-11-23 00:28:17 -0300

    Feito pagina de ativação de conta

--- ativarconta.php
+++ ativarconta.php
@@ -1,7 +1,6 @@
 <?php
 
 require './db.php';
-require_once './check.php';
 require_once './getuserdata.php';
 
 function check_expiration_time($expiration_time){
@@ -34,3 +33,27 @@ if ($data && ! $data["is_active"]){
 }
 
 $db = null;
+session_start();
+require_once './check.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ATIVAR CONTA</title>
+    <link rel="stylesheet" href="./style/style.css">
+    <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <script src="js/validate.js"></script>
+    <script src="js/buttons.js"></script>
+    <script src="js/redirect.js"></script>
+    <?php require './fonts'?>
+</head>
+<body class="redirect">
+    <?php require './navbar.php'?>
+    <img src="./siteimages/loginicon.png" class ="logo">
+    <p>Sua conta foi ativada. Você será redirecionado automaticamente.</p>
+</body>
+</html>
